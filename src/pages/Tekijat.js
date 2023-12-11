3648a8d36ea392a430558b487f63c77433eb1d9b
Sabrina <sabrinadesireee@gmail.com>
2023-12-11 14:58:56 +0200

    Tekijätsivu ja kuvat päivitetty

--- src/pages/Tekijat.js
+++ src/pages/Tekijat.js
@@ -3,7 +3,9 @@ import React from 'react'
 export default function Home() {
   return (
     <div className='content'>
-      <h1>Tekijät</h1>
+      <h1>Verkkopalveluprojekti syksyn kurssi 2023</h1>
+      <h3>Projektissa mukana olleet henkilöt:</h3>
+      <h4>Atte Huikuri, Sabrina Davidsson, Roosa Nieminen, Jussi Molin, Jarkko Määttä, Lisa Törmänen</h4>
     </div>
   )
 }
